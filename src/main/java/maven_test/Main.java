1a30eab6727db1a0cbee27bb245a0fc0524d6656
alisnake62 <79987276+alisnake62@users.noreply.github.com>
2022-03-08 14:52:26 +0100

    Update Main.java

--- src/main/java/maven_test/Main.java
+++ src/main/java/maven_test/Main.java
@@ -86,7 +86,7 @@ public class Main {
         String row_materiel = "<div>var_materiel</div>";
         String fiche_materiel = "";
         String row_mat = row_materiel;
-        row_mat = row_mat.replace("var_materiel", "<br>" + user_staff.get(4));
+        row_mat = row_mat.replace("var_materiel", "<br>" + user_staff.get(4) + test);
         fiche_materiel = fiche_materiel + row_mat;
 
         String new_fiche_html = index_html.
