fbf46c1556a1f2129b7e9cecebc4f20d3dcbcc91
alisnake62 <79987276+alisnake62@users.noreply.github.com>
2022-03-10 09:56:11 +0100

    Update Main.java

--- src/main/java/maven_test/Main.java
+++ src/main/java/maven_test/Main.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
+//comment
 public class Main {
 
 	public static void main(String[] args) throws IOException {
